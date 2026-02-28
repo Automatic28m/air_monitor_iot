5eedc7715d09586aa27b66507bdd6a2d987e0088
Phanlop Boonluea <phanlop@192.168.1.191>
2026-02-28 16:59:08 +0700

    update import path

--- air_monitor_iot/src/app/api/settings/route.js
+++ air_monitor_iot/src/app/api/settings/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import connectMongo from '@/lib/mongodb';
-import Settings from '@/models/Settings';
+import connectMongo from '../../../lib/mongodb';
+import Settings from '../../../models/Settings';
 
 export async function GET() {
     try {
